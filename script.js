e48ff7b54b3aeefe895de5ac29de6e54b8935a65
Carter <carter@pangweb.com>
2023-05-10 21:45:48 -0700

    city changes

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var weatherInp = document.getElementById("weather-input");
 var searchBut = document.getElementById("search");
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?"
 var APIkey = "869e9a32e52cb52888984677937a5d9c";
-var temp;
-var wc;
-var city;
-var humid;
-var windspd;
+var temp = document.getElementById("temp");
+var wc = document.getElementById("wc");
+var city = document.getElementById("city");
+var humid = document.getElementById("humid");
+var windspd = document.getElementById("windspd");
 var latLi = document.getElementById("lat");
 var lonLi = document.getElementById("lon");
 
@@ -28,21 +28,37 @@ fetch(requestURL)
     lonLi.textContent = data[0].lon;
     console.log(latLi);
     getWeather(data);
+    //fiveForecast(data);
 }
 )
-
+function fiveForecast(){
+    var forecastURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+    fetch(forecastURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+}
 // 2nd API call using LAT and LON
 function getWeather(data){
     var lat = data[0].lat;
     var lon = data[0].lon;
-    var weatherURL = queryURL + "lat="+lat+"lon="+lon +"&appid="+ APIkey;
+    var weatherURL = queryURL + "lat="+lat+"&lon="+lon +"&appid="+ APIkey;
     console.log(weatherURL);
     fetch(weatherURL)
     .then(function (response){
         return response.json();
     })
     .then(function (data){
-        console.log(data);
+        console.log(data);// show attributes here
+        city.textContent = data.name;
+        temp.textContent = data.main.temp;
+        wc.textContent = data.weather[0].icon;
+        humid.textContent = data.main.humidity;
+        windspd.textContent = data.wind.speed;
+        console.log(humid);
     })
 }
 }
